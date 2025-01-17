4c4b4de9330b0b79b2bed0cfd71907f59f644754
Kacper Jóźwik <kacperjozwik12@gmail.com>
2025-01-17 21:18:55 +0100

    implentacja themingu | dark mode/light mode

---
+++ frontend/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/ui/ModeToggle"
+
+export default function Home() {
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  )
+}
