74d8b426c58fefa0f39b9ad6d6811bbf6297561f
Onestyle7 <klusbartek8@gmail.com>
2025-01-19 20:14:50 +0100

    Dodanie dataAnnotations do Dto odpowiadającego za logowanie użytkownika

--- backend/Data/Dtos/LoginRequestDto.cs
+++ backend/Data/Dtos/LoginRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace backend.Data.Dtos
 {
     public class LoginRequestDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [MinLength(6, ErrorMessage = "Hasło musi mieć co najmniej 6 znaków")]
         public string Password { get; set; }
     }
 }
