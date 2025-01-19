8dfbf9f079130f3e0606817616f2fc0b52c4ca3f
Onestyle7 <klusbartek8@gmail.com>
2025-01-19 20:20:36 +0100

    Poprawienie interfejsu do przyjmowania parametrów z Dto

--- backend/Services/ILoginService.cs
+++ backend/Services/ILoginService.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Data.Dtos;
 using backend.Data.Models;
 
 namespace backend.Services
 {
     public interface ILoginService
     {
-        Task<User?> AuthenticateUserAsync(string email, string password);
+        Task<User?> AuthenticateUserAsync(LoginRequestDto loginRequest);
         string GenerateJwtToken(User user);
     }
 }
