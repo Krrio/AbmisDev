8a548819b270ba985c2f5db44b16a898b28bbb80
Onestyle7 <klusbartek8@gmail.com>
2025-01-21 17:04:07 +0100

    Poprawienie przyjmowanych parametrów w metodach przez dodanie userID w zelu autoryzacji Zadań użytkowników

--- backend/Services/ITasksService.cs
+++ backend/Services/ITasksService.cs
@@ -9,11 +9,11 @@ namespace backend.Services
 {
     public interface ITasksService
     {
-        Task<IEnumerable<ToDoItem>> GetAllTasksAsync();
-        Task<ToDoItem> GetTaskByIdAsync(int id);
-        Task<ToDoItem> CreateTaskAsync(ToDoTaskRequestDto request);
-        Task<ToDoItem> DeleteTaskAsync(int id);
-        Task<ToDoItem> UpdateTaskAsync(int id, ToDoTaskRequestDto request);
-
+        Task<IEnumerable<ToDoItem>> GetAllTasksAsync(int userId);
+        Task<ToDoItem> GetTaskByIdAsync(int id, int userId);
+        Task<ToDoItem> CreateTaskAsync(ToDoTaskRequestDto request, int userId);
+        Task<ToDoItem> DeleteTaskAsync(int id, int userId);
+        Task<ToDoItem> UpdateTaskAsync(int id, ToDoTaskRequestDto request, int userId);
+        Task<IEnumerable<ToDoItem>> GetTasksByDateAsync(DateTime date, int userId);
     }
 }
