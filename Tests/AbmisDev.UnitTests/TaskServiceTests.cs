6b5b9d39d60f76b30132c1256343bec5e2272273
Onestyle7 <klusbartek8@gmail.com>
2025-01-30 19:17:14 +0100

    Testy: Zaimplementowanie testów jednostkowych do TasksService(CRUD + filtrowanie)
    Dodanie unit testów do metod:
    - CreateTaskAsync
    - DeleteTaskAsync
    - GetTaskByIdAsync
    - UpdateTaskAsync
    - GetTasksByDateAsync

--- Tests/AbmisDev.UnitTests/TaskServiceTests.cs
+++ Tests/AbmisDev.UnitTests/TaskServiceTests.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.Data;
+using backend.Data.Dtos;
 using backend.Data.Models;
+using backend.Data.Models.Enums;
 using backend.Enums;
 using backend.Services;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Xunit;
+using Xunit.Sdk;
 
 namespace AbmisDev.UnitTests
 {
@@ -137,6 +142,321 @@ namespace AbmisDev.UnitTests
                 // Assert
                 result.Should().BeInDescendingOrder(t => t.DueDate);
                 result.First().DueDate.Should().Be(fixedDate.AddDays(5)); // 15.10
+            }
+        }
+        [Fact]
+        public async Task CreateTaskWithValidData_ReturnsNewTask()
+        { 
+            // Arrange
+            int userId = 1;
+            var options = GetDbOptions();
+
+            using (var context = new AppDbContext(options))
+            {
+                var service = new TasksService(context);
+
+                var taskRequest = new ToDoTaskRequestDto
+                {
+                    Title = "Test task",
+                    DueDate = DateTime.UtcNow,
+                    Priority = ItemPriority.High
+                };
+
+                // Act
+                var result = await service.CreateTaskAsync(taskRequest, userId);
+
+                // Assert
+                result.Should().NotBeNull();
+                result.Title.Should().Be("Test task");
+                result.UserId.Should().Be(userId);
+                result.DueDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(3)); // Upewniamy się, że data jest poprawna
+                result.ItemPriority.Should().Be(ItemPriority.High);
+            }
+        }
+        [Fact]
+        public async Task CreateTaskWithNoProvidedPriority_ReturnsMedium()
+        {
+            //Arrange
+            int userId = 1;
+            var options = GetDbOptions();
+            
+            using(var context = new AppDbContext(options))
+            {
+                var service = new TasksService(context);
+
+                var taskRequest = new ToDoTaskRequestDto
+                {
+                    Title = "Test task",
+                    DueDate = DateTime.UtcNow
+                };
+
+                //Act 
+                var result = await service.CreateTaskAsync(taskRequest, userId);
+
+                //Assert
+                result.Should().NotBeNull();
+                result.Title.Should().Be("Test task");
+                result.UserId.Should().Be(userId);
+                result.DueDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+                result.ItemPriority.Should().Be(ItemPriority.Medium);
+            }
+        }
+        [Fact]
+        public async Task CreateTaskWithNoProvidedDueDate_ReturnsNow()
+        {
+            //Arrange
+            int userId = 1;
+            var options = GetDbOptions();
+
+            using(var context = new AppDbContext(options))
+            {
+                var service = new TasksService(context);
+
+                var taskRequest = new ToDoTaskRequestDto
+                {
+                    Title = "Test task"
+                };    
+
+                //Act 
+                var result = await service.CreateTaskAsync(taskRequest, userId);
+
+                //Assert
+                result.Should().NotBeNull();
+                result.Title.Should().Be("Test task");
+                result.UserId.Should().Be(userId);
+                result.DueDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+                result.ItemPriority.Should().Be(ItemPriority.Medium);
+            }
+        }
+        [Fact]
+        public async Task DeleteTaskAsync_RemovesTask_WhenTaskExists()
+        {
+            // Arrange
+            int userId = 1;
+            var options = GetDbOptions();
+
+            using(var context = new AppDbContext(options))
+            {
+                var service = new TasksService(context);
+
+                var task = new ToDoItem
+                {
+                    Title = "Test task",
+                    DueDate = DateTime.UtcNow,
+                    UserId = userId
+                };
+
+                context.Tasks.Add(task);
+                await context.SaveChangesAsync();
+                // Act
+                var result = await service.DeleteTaskAsync(task.Id, userId);
+
+                // Assert
+                result.Should().NotBeNull();
+                result.Id.Should().Be(task.Id);
+                result.Title.Should().Be("Test task");
+
+                var deletedTask = await context.Tasks.FindAsync(task.Id);
+                deletedTask.Should().BeNull();
+            }
+        }
+        [Fact]
+        public async Task GetTaskByIdAsync_ReturnsTask_WhenTaskExists()
+        {
+            int userId = 1;
+            var options = GetDbOptions();
+
+            using(var context = new AppDbContext(options))
+            {
+                var service = new TasksService(context);
+
+                var task = new ToDoItem
+                {
+                    Title = "Test task",
+                    DueDate = DateTime.UtcNow,
+                    UserId = userId
+                };
+
+                context.Tasks.Add(task);
+                await context.SaveChangesAsync();
+
+                //Act
+                var result = await service.GetTaskByIdAsync(task.Id, userId);
+
+                //Assert
+                result.Should().NotBeNull();
+                result.Id.Should().Be(task.Id);
+                result.Title.Should().Be("Test task");
+
+            }
+        }
+        [Fact]
+        public async Task GetTaskByIdAsync_ReturnsException_WhenTaskDoesNotExist()
+        {
+            int userId = 1;
+            var options = GetDbOptions();
+
+            using(var context = new AppDbContext(options))
+            {
+                var service = new TasksService(context);
+
+                //Act & Assert
+
+                await FluentActions
+                    .Invoking(() => service.GetTaskByIdAsync(1, userId))
+                    .Should().ThrowAsync<KeyNotFoundException>();
+                
+            }
+        }
+        [Fact]
+        public async Task UpdateTaskAsync_UpdatesTask_WhenTaskExists()
+        {
+            // Arrange
+            int userId = 1;
+            var options = GetDbOptions();
+
+            using(var context = new AppDbContext(options))
+            {
+                var service = new TasksService(context);
+
+                var task = new ToDoItem
+                {
+                    Title = "Test task",
+                    DueDate = DateTime.UtcNow,
+                    UserId = userId
+                };
+
+                context.Tasks.Add(task);
+                await context.SaveChangesAsync();
+
+                var taskRequest = new ToDoTaskRequestDto
+                {
+                    Title = "Updated task",
+                    DueDate = DateTime.UtcNow.AddDays(1),
+                    Priority = ItemPriority.High
+                };
+                
+                // Act
+                var result = await service.UpdateTaskAsync(task.Id, taskRequest, userId);
+
+                // Assert
+                result.Should().NotBeNull();
+                result.Id.Should().Be(task.Id);
+                result.Title.Should().Be("Updated task");
+                result.DueDate.Should().BeCloseTo(DateTime.UtcNow.AddDays(1), TimeSpan.FromSeconds(1));
+                result.ItemPriority.Should().Be(ItemPriority.High);
+            }
+        }
+        [Fact]
+        public async Task UpdateTaskAsync_ReturnsException_WhenTaskDoesNotExist()
+        {
+            // Arrange
+
+            int userId = 1;
+            var options = GetDbOptions();
+
+            using(var context = new AppDbContext(options))
+            {
+                var service = new TasksService(context);
+
+                var taskRequest = new ToDoTaskRequestDto
+                {
+                    Title = "Updated task",
+                    DueDate = DateTime.UtcNow.AddDays(1),
+                    Priority = ItemPriority.High
+                };
+
+                //Act & Assert
+                await FluentActions
+                    .Invoking(() => service.UpdateTaskAsync(1, taskRequest, userId))
+                    .Should().ThrowAsync<KeyNotFoundException>();
+            }
+        }
+        [Fact]
+        public async Task GetTasksByDateAsync_ReturnsOnlyProvidedDate()
+        {
+            int userId = 1;
+            var options = GetDbOptions();
+
+            using(var context = new AppDbContext(options))
+            {
+                var service = new TasksService(context);
+                var today = DateTime.UtcNow;
+
+                context.Tasks.Add(new ToDoItem
+                {
+                    Title = "Test task",
+                    DueDate = DateTime.UtcNow,
+                    UserId = userId
+                });
+
+                context.Tasks.Add(new ToDoItem
+                {
+                    Title = "Test task",
+                    DueDate = today.AddDays(1),
+                    UserId = userId
+                });
+
+                context.Tasks.Add(new ToDoItem
+                {
+                    Title = "Test task",
+                    DueDate = today.AddDays(-1),
+                    UserId = userId
+                });
+
+                await context.SaveChangesAsync();
+
+                // Act
+
+                var result = await service.GetTasksByDateAsync(today, userId);
+
+                // Assert
+                result.Should().HaveCount(1);
+                result.First().Title.Should().Be("Test task");
+                result.First().DueDate.Should().BeCloseTo(today, TimeSpan.FromSeconds(1));
+
+            }
+        }
+        [Fact]
+        public async Task GetTasksByDateAsync_returnsEmptyList_WhenNoTasksExist()
+        {
+            int userId = 1;
+            var options = GetDbOptions();
+
+            using(var context = new AppDbContext(options))
+            {
+                var service = new TasksService(context);
+                var today = DateTime.UtcNow.Date;
+
+                context.Tasks.Add(new ToDoItem
+                {
+                    Title = "Test task",
+                    DueDate = today.AddDays(140),
+                    UserId = userId
+                });
+
+                context.Tasks.Add(new ToDoItem
+                {
+                    Title = "Test task",
+                    DueDate = today.AddDays(1),
+                    UserId = userId
+                });
+
+                context.Tasks.Add(new ToDoItem
+                {
+                    Title = "Test task",
+                    DueDate = today.AddDays(-1),
+                    UserId = userId
+                });
+
+                await context.SaveChangesAsync();
+
+                // Act
+                var result = await service.GetTasksByDateAsync(today, userId);
+
+                // Assert
+                result.Should().BeEmpty();
+
             }
         }
     }
